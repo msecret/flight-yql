b4f7abbe81e29f705bb540542bfa835114e6a232
Marco Secreto <msegreto@miceover.com>
2014-06-22 11:10:31 -0700

    Add integration test for the data sent back
    Fixes #4
    
    Added integration test that actually checks that data sent back is real
    and makes sense. Kept this test relatively future proof in case the YAHOO
    response changes.
    
    Other Changes:
    - Added more sinon and jasmine things to jshintrc

--- test/integration/flight-yql.spec.js
+++ test/integration/flight-yql.spec.js
@@ -2,7 +2,13 @@
 
 describeComponent('lib/flight-yql', function () {
   beforeEach(function () {
+    var self = this;
     setupComponent();
+
+    this.data = {};
+    this.callback = function(data) {
+      self.data = data; 
+    };
   });
 
   it('should call the callback passed in on success', function() {
@@ -18,6 +24,26 @@ describeComponent('lib/flight-yql', function () {
 
     runs(function() {
       expect(testCallback).toHaveBeenCalled();
+    });
+  });
+  it('should call the callback passed in on success', function() {
+    var testCallback = spyOn(this, 'callback').andCallThrough(),
+        self = this;
+
+    var s = this.component.query(
+        'select * from yahoo.finance.stocks where symbol="yhoo"',
+        testCallback);
+
+    waitsFor(function() {
+      return testCallback.callCount > 0;
+    }, "The Ajax call timed out.", 5000);
+
+    runs(function() {
+      var data = self.data.query;
+
+      expect(data.count).toEqual(1);
+      expect(data.results).toBeDefined();
+      expect(data.results.stock.symbol.toLowerCase()).toEqual('yhoo');
     });
   });
 });
